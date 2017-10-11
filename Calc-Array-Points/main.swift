2465e3b62375fc12489dbd5b910489617b9e548b
Naruth Kongurai <naruth1212@gmail.com>
2017-10-11 13:32:04 -0700

    Initial Commit

---
+++ Calc-Array-Points/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Calc-Array-Points
+//
+//  Created by Naruth Kongurai on 10/11/17.
+//  Copyright © 2017 Naruth Kongurai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
